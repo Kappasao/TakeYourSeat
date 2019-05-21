3dd27a08af39245adab5bf9de912af58b38f00c4
Manel Sitjar <manel.sitjar@gmail.com>
2019-05-21 16:16:01 +0200

    rectificacion orden de las pantallas

---
+++ TakeYourSeat/NumComensales.swift
@@ -0,0 +1,13 @@
+//
+//  NumComensales.swift
+//  TakeYourSeat
+//
+//  Created by Manel Sitjar on 17/05/2019.
+//  Copyright © 2019 AlvaroManelTaniaUri. All rights reserved.
+//
+
+import Foundation
+class NumComensales {
+    
+    
+}
