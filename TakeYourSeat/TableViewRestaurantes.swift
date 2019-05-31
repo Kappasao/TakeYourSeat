c66dbed813bee6dc2958781c5009a5b6b342f807
alu2016394 <alvarosalomoni.97@gmail.com>
2019-05-31 16:12:04 +0200

    Connection to bbdd

--- TakeYourSeat/TableViewRestaurantes.swift
+++ TakeYourSeat/TableViewRestaurantes.swift
@@ -30,7 +30,7 @@ class TableViewRestaurantes : UIViewController, UITableViewDataSource, UITableVi
         
         //myCell.restName.text = restComerAqui[indexPath.row].name
         myCell.restName.text = postDataRef[indexPath.row].name
-        myCell.restImg.image = postDataRef[indexPath.row].image
+        //myCell.restImg.image = postDataRef[indexPath.row].image
         myCell.restLoc.text = postDataRef[indexPath.row].location
         //let URL = URL(postDataRef[indexPath.row])
         //myCell.restImg.image =
@@ -67,23 +67,11 @@ class TableViewRestaurantes : UIViewController, UITableViewDataSource, UITableVi
                 print(name)
             }
         }*/
+
+        getDatabaseData()
         
-        
-        /*
-         _commentsRef.observe(.value) { snapshot in
-         for child in snapshot.children {
-         ...
-         }
-         }
-         */
-        
-        ref.observe(.value) { (snapshot) in
-            for child in snapshot.children{
-                
-            }
-        }
-        
-        ref.child("restaurants").child("Can Stucom").observeSingleEvent(of: .value, with: { (snapshot) in
+       
+        /*ref.child("restaurants").child("Can Stucom").observeSingleEvent(of: .value, with: { (snapshot) in
                 // Get user value
             
             
@@ -103,7 +91,33 @@ class TableViewRestaurantes : UIViewController, UITableViewDataSource, UITableVi
                 print(error.localizedDescription)
         }
         self.tableView.reloadData()
-        
+        */
+    }
+    
+    func getDatabaseData(){
+        let idRef = self.ref.child("restaurants")
+        idRef.observe(DataEventType.value, with: {
+            (snapshot) in
+                for restaurants in snapshot.children.allObjects as! [DataSnapshot]{
+                    let value = restaurants.value as? [String: AnyObject]
+                    
+                    let name = value?["Name"]
+                    let url = value?["image"]
+                    let location = value?["location"]
+                    print(name)
+                    print(location)
+                    print(url)
+                    let Url = URL(string: url as! String)
+                    if let data = try? Data(contentsOf: Url!) {
+                        let imagen: UIImage = (UIImage(data: data) ?? nil)!
+                        //self.postDataRef.append(Restaurante(name: name!, image: imagen, location: location!))
+                        self.postDataRef.append(Restaurante(name: name as! String, image: imagen, location: location as! String))
+                    }
+                    //let imagen = UIImage(named: "100")
+                    //self.postDataRef.append(Restaurante(name: name as! String, image: imagen!, location: location as! String))
+                }
+            self.tableView.reloadData()
+        })
     }
     
     
